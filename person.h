6c2d814db142762da07a57a56f3260e2a14b34a8
Ishti1 <ishtiak7070@gmail.com>
2026-02-21 23:44:14 +0600

    person.h

--- person.h
+++ person.h
@@ -9,15 +9,22 @@ protected:
     string id;
     string name;
     string password;
+    static int count;
 public:
-    Person(string i = "", string n = "", string pass = "");
-
+    Person(string i = "", string n = "", string pass = "")
+          : id(i),name(n), passsword(p) {}
+    Person(string i = "", string n = "", string pass = "")
     string getId();
     string getName();
+    bool checkPassword(string p)
+   {
+       return p==pasword;
+   }
     bool checkPassword(string p);
 
-    // allow derived classes to override for polymorphic display
-    virtual void display();
+    virtual void display()=0;
+    static int getCount() { return count;}
 };
 
 #endif
+
