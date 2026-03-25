a7a8cd599877b7cc1334221cde6b82eac9310581
Ishti1 <ishtiak7070@gmail.com>
2026-03-25 12:14:19 +0600

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,27 +1,22 @@
-#include "Person.h"
+#include "person.h"
+#include <bits/stdc++.h>
+using namespace std;
 
 int Person::count = 0;
-Person::Person(string i, string n, string pass) {
-    id = i;
-    name = n;
-    password = pass;
-    count++
-}
-Person:: ~Person() {
-        count--;
+
+Person::Person(string i, string n, string pass)
+    : id(i), name(n), password(pass) {
+    count++;
 }
 
-string Person::getId() { return id; }
-string Person::getName() { return name; }
-
-bool Person::checkPassword(string p) {
-    return p == password;
-}
-string Person::toCSV() const
-{
-    return getName() + "," + getId();
+Person::~Person() {
+    count--;
 }
 
+string Person::getId()       const { return id; }
+string Person::getName()     const { return name; }
+string Person::getPassword() const { return password; }
 
+void Person::setPassword(const string& p) { password = p; }
 
-
+bool Person::checkPassword(string p) const { return p == password; }
