af30a2805779c82009f8b1375ab710e560de99f6
Ishti1 <ishtiak7070@gmail.com>
2026-03-25 12:12:52 +0600

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,7 +1,7 @@
 #ifndef ADMIN_H
 #define ADMIN_H
 
-#include "Person.h"
+#include "person.h"
 class System;
 
 class Admin : public Person {
@@ -12,19 +12,23 @@ private:
 public:
     Admin(string id, string name, string pass, System *s);
     void display() override;
+
     bool login(string pass);
 
-    void addStudent(string id, string name);
+    void addStudent(string id, string name,string sec, string dept);
     void addFaculty(string id, string name);
     void addCourse(string id, string name);
     void assignFacultyToCourse(string facultyID, string courseID);
     void setCreditLimit(int limit);
     void manageCapacity(string courseID, int newCapacity);
-    
-
+    void deleteStudent(string id);
+    void modifyStudentInfo(string id);
+    void modifyCourseInfo(string courseID);
+    void deleteFaculty(string id);
     void enrollStudent(string studentID, string courseID);
+    void markCoursesEquivalent(string courseA, string courseB);
+    void setStudentProgress(string studentID);
     void viewAll();
 };
 
 #endif
-
