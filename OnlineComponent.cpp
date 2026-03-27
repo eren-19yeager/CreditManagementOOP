12955e14dfc6eaee6f5b3c21edcdd92ffd172223
eren-19yeager <jaymatasnim@iut-dhaka.edu>
2026-03-27 16:16:37 +0600

    Update OnlineComponent.cpp

--- OnlineComponent.cpp
+++ OnlineComponent.cpp
@@ -1,93 +1,53 @@
 #include "OnlineComponent.h"
-
+#include <bits/stdc++.h>
+using namespace std;
 
 OnlineComponent::OnlineComponent(string id)
-: CourseComponent(id),
-  TheoryComponent(id),
-  LabComponent(id),
-  theoryWeight(0.7f),
-  labWeight(0.3f)
-{
+    : CourseComponent(id), TheoryComponent(id), LabComponent(id),
+      theoryWeight(0.6f), labWeight(0.4f) {}
+
+void OnlineComponent::enroll() {
+    TheoryComponent::enroll();
+    LabComponent::enroll();
+    cout << "Online Component " << componentID << " fully enrolled.\n";
 }
 
-
-
-void OnlineComponent::enroll()
-{
-    cout << "Online Integrated Enrollment completed for "
-         << componentID << endl;
-}
-
-
-
-void OnlineComponent::evaluate(float)
-{
-    calculateResult();
-    evaluated = true;
-
-    cout << "Online integrated evaluation completed for "
-         << componentID << endl;
-}
-
-void OnlineComponent::calculateResult()
-{
-    
-
+void OnlineComponent::calculateResult() {
     TheoryComponent::calculateResult();
-    float theoryRes = TheoryComponent::getResult();
+    float theoryResult = result;
 
     LabComponent::calculateResult();
-    float labRes = LabComponent::getResult();
+    float labResult = result;
 
-   
-    result = theoryRes * theoryWeight +
-             labRes * labWeight;
+    result = (theoryResult * theoryWeight) + (labResult * labWeight);
 }
 
-
-
-string OnlineComponent::getComponentType() const
-{
-    return "Online Integrated Component";
+void OnlineComponent::evaluate(float) {
+    calculateResult();
+    evaluated = true;
+    cout << "Online evaluation completed for " << componentID << "\n";
 }
 
-
-
-void OnlineComponent::displayIntegratedBreakdown() const
-{
-    cout << "\nIntegrated Evaluation Breakdown\n";
-
-    cout << "Theory Weight: "
-         << theoryWeight * 100 << "%\n";
-
-    cout << "Lab Weight: "
-         << labWeight * 100 << "%\n";
+string OnlineComponent::getComponentType() const {
+    return "Online Component (Theory + Lab)";
 }
 
-void OnlineComponent::displayStatus() const
-{
-    cout << "\n===== ONLINE COMPONENT REPORT =====\n";
+void OnlineComponent::displayIntegratedBreakdown() const {
+    TheoryComponent::displayBreakdown();
+    LabComponent::displayBreakdown();
+    cout << "\nWeighting: Theory " << (theoryWeight * 100)
+         << "% / Lab " << (labWeight * 100) << "%\n";
+}
 
-    cout << "Component ID: "
-         << componentID << endl;
-
-    cout << "Type: "
-         << getComponentType() << endl;
-
+void OnlineComponent::displayStatus() const {
+    cout << "\n===== ONLINE STATUS REPORT =====\n";
+    cout << "Component ID: " << componentID        << "\n";
+    cout << "Type: "         << getComponentType() << "\n";
     displayIntegratedBreakdown();
-
-    cout << "Final Combined Result: "
-         << result << "%\n";
-
-    cout << "Evaluation Status: "
-         << (evaluated ? "Completed" : "Pending")
-         << endl;
+    cout << "Weighted Result:  " << result << "\n";
+    cout << "Evaluation Status: " << (evaluated ? "Completed" : "Pending") << "\n";
 }
 
-
-
-OnlineComponent::~OnlineComponent()
-{
-    cout << "Destroying OnlineComponent: "
-         << componentID << endl;
+OnlineComponent::~OnlineComponent() {
+    cout << "Destroying OnlineComponent: " << componentID << "\n";
 }
