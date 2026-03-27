ea58d59d4a468b5cdeddcc1a2e51d03af7a755ca
eren-19yeager <jaymatasnim@iut-dhaka.edu>
2026-03-27 16:05:19 +0600

    Update Grades.h

--- Grades.h
+++ Grades.h
@@ -1,21 +1,43 @@
 #ifndef GRADES_H
 #define GRADES_H
+
 #include <string>
+#include <iostream>
 using namespace std;
 
+class CourseComponent;
+
 class Grades {
-private:
-    float percentage;
-    float gradePoint;
+public:
+    string studentID;
+    string courseCode;
+    float  marks;
     string letter;
 
-public:
-    Grades(float p = 0);
+    CourseComponent* component;
+
+    Grades();
+    Grades(string sid, string code, float m);
+    Grades(string sid, string code, CourseComponent* comp);
 
     void computeGrade();
+    void display() const;
 
-    float getGPA() const;
-    string getLetter() const;
+    // Sort grades by marks (highest first)
+    bool operator< (const Grades &other) const { return marks >  other.marks; }
+    bool operator==(const Grades &other) const {
+        return studentID == other.studentID && courseCode == other.courseCode;
+    }
+
+    //Serialization
+    void serialize(ofstream &out) const;
+    static Grades deserialize(const string &line);
+
+    // Stream output: prints "courseCode : marks (letter)"
+    friend ostream& operator<<(ostream &os, const Grades &g) {
+        os << g.courseCode << " : " << g.marks << " (" << g.letter << ")";
+        return os;
+    }
 };
 
 #endif
