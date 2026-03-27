06984961d104874e22ab78fe63193db749a2cdd1
eren-19yeager <jaymatasnim@iut-dhaka.edu>
2026-03-27 16:21:05 +0600

    Update TheoryComponent.h

--- TheoryComponent.h
+++ TheoryComponent.h
@@ -3,16 +3,6 @@
 
 #include "CourseComponent.h"
 
-/*
-    Theory Component Evaluation
-
-    Structure:
-        Quizzes (4)
-        Assignment
-        Mid Exam
-        Final Exam
-*/
-
 class TheoryComponent : virtual public CourseComponent {
 private:
     float quizzes[4];
@@ -21,31 +11,18 @@ private:
     float finalExam;
 
 public:
-
-  
     TheoryComponent();
     TheoryComponent(string id);
 
-   
     void enroll();
-
-  
-    void setTheoryMarks(float q[],
-                        float assign,
-                        float mid,
-                        float finalM);
-
+    void setTheoryMarks(float q[], float assign, float mid, float finalM);
     void calculateResult();
 
-    
-    void evaluate(float dummy = 0) override;
-    string getComponentType() const override;
+    void   evaluate(float dummy = 0) override;
+    string getComponentType()        const override;
+    void   displayStatus()           const override;
+    void   displayBreakdown()        const;
 
-   
-    void displayStatus() const override;
-    void displayBreakdown() const;
-
-  
     ~TheoryComponent();
 };
 
