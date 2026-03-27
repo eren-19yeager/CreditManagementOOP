dbfa0c4929e69b4d3cf05535624b2f5eb9927393
na-rai <nazraisa2510@gmail.com>
2026-03-27 16:26:04 +0600

    Update ResultContribution.cpp

--- ResultContribution.cpp
+++ ResultContribution.cpp
@@ -1,13 +1,1 @@
-#include "ResultContribution.h"
 
-ResultContribution::ResultContribution(float cg, float c) {
-    creditGpa = cg;
-    credits = c;
-}
-
-ResultContribution ResultContribution::operator+(const ResultContribution& other) {
-    return ResultContribution(
-        creditGpa + other.creditGpa,
-        credits + other.credits
-    );
-}
