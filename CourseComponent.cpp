969ea8b7f0b2ae59a0a8bdf675ec49a18407dd9a
eren-19yeager <jaymatasnim@iut-dhaka.edu>
2026-03-27 15:58:31 +0600

    Update CourseComponent.cpp

--- CourseComponent.cpp
+++ CourseComponent.cpp
@@ -1,109 +1,69 @@
 #include "CourseComponent.h"
-
+#include <bits/stdc++.h>
+using namespace std;
 
 CourseComponent::CourseComponent()
-    : componentID("NA"),
-      result(0),
-      fullMarks(100),
-      evaluated(false)
-{
-}
+    : componentID("NA"), result(0), fullMarks(100), evaluated(false) {}
 
 CourseComponent::CourseComponent(string id, float full)
-    : componentID(id),
-      result(0),
-      fullMarks(full),
-      evaluated(false)
-{
-}
+    : componentID(id), result(0), fullMarks(full), evaluated(false) {}
 
 CourseComponent::CourseComponent(string id, float res, float full)
-    : componentID(id),
-      result(res),
-      fullMarks(full),
-      evaluated(true)
-{
-}
-
-
+    : componentID(id), result(res), fullMarks(full), evaluated(true) {}
 
 void CourseComponent::evaluate(float marks)
 {
-    if(marks < 0 || marks > fullMarks)
+    if (marks < 0 || marks > fullMarks)
     {
-        cout << "Invalid marks entered for "
-             << componentID << endl;
+        cout << "Invalid marks entered for " << componentID << "\n";
         return;
     }
-
     result = marks;
     evaluated = true;
-
-    cout << "Component " << componentID
-         << " evaluated successfully.\n";
+    cout << "Component " << componentID << " evaluated successfully.\n";
 }
 
 float CourseComponent::calculatePercentage() const
 {
-    if(fullMarks == 0)
+    if (fullMarks == 0)
         return 0;
-
     return (result / fullMarks) * 100.0f;
 }
-
-
 
 void CourseComponent::displayStatus() const
 {
     cout << "\nComponent Report\n";
-    cout << "ID: " << componentID << endl;
-    cout << "Type: " << getComponentType() << endl;
-    cout << "Marks: " << result
-         << "/" << fullMarks << endl;
-
-    cout << "Percentage: "
-         << calculatePercentage()
-         << "%\n";
-
-    cout << "Status: "
-         << (evaluated ? "Evaluated" : "Pending")
-         << endl;
+    cout << "ID: " << componentID << "\n";
+    cout << "Type: " << getComponentType() << "\n";
+    cout << "Marks: " << result << "/" << fullMarks << "\n";
+    cout << "Percentage: " << calculatePercentage() << "%\n";
+    cout << "Status: " << (evaluated ? "Evaluated" : "Pending") << "\n";
 }
 
 void CourseComponent::resetMarks()
 {
     result = 0;
     evaluated = false;
-
-    cout << "Marks reset for "
-         << componentID << endl;
+    cout << "Marks reset for " << componentID << "\n";
 }
 
-
-string CourseComponent::getComponentID() const
-{
+string CourseComponent::getComponentID() const { 
     return componentID;
 }
 
-float CourseComponent::getResult() const
-{
-    return result;
+float CourseComponent::getResult() const { 
+return result;
 }
 
-float CourseComponent::getFullMarks() const
-{
-    return fullMarks;
+float CourseComponent::getFullMarks() const { 
+return fullMarks; 
 }
 
-bool CourseComponent::isEvaluated() const
-{
-    return evaluated;
+bool CourseComponent::isEvaluated() const { 
+return evaluated; 
 }
-
-
 
 CourseComponent::~CourseComponent()
 {
-    cout << "Destroying CourseComponent: "
-         << componentID << endl;
+    cout << "Destroying CourseComponent: " << componentID << "\n";
 }
