f212ebb6b15ae210182804bbfde957c4fa03954c
na-rai <nazraisa2510@gmail.com>
2026-03-27 16:26:32 +0600

    Update ResultContribution.h

--- ResultContribution.h
+++ ResultContribution.h
@@ -1,15 +1,1 @@
-#ifndef RESULTCONTRIBUTION_H
-#define RESULTCONTRIBUTION_H
 
-class ResultContribution {
-public:
-    float creditGpa;   // ci * gpi
-    float credits;
-
-    ResultContribution(float cg=0, float c=0);
-
-    // operator overloading
-    ResultContribution operator+(const ResultContribution& other);
-};
-
-#endif
